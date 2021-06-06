1c5699693c12f8d1be27f921d647aca1dd71f762
hanagatasachie <73112533+1543rrr@users.noreply.github.com>
2021-06-07 02:37:56 +0900

    ゲストログインが出来るようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   has_many :rooms, through: :room_users  
   has_many :posts 
   has_many :comments 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      user.nickname = "ゲスト" 
+    end
+  end
 end
