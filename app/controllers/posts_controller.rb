f0ce67c785862d9af971d0ac0263ff5a5879a9b9
hanagatasachie <73112533+1543rrr@users.noreply.github.com>
2021-06-07 00:34:23 +0900

    新規投稿ページに遷移するようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
   
-  def create 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+    render :new
+    end    
   end  
 
+  private
+
+  def post_params
+    params.require(:post).permit(:fes_name, :impressions, :image).merge(user_id: current_user.id)
+  end
+
 end
+
